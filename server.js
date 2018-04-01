cf0a50d4a8ecb0529620f681e6ec280eb0cec32d
Donald Dong <xdong@csumb.edu>
2018-03-31 22:39:16 -0700

    added CORS

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ const twitter = new OAuth.OAuth(
 
 app.use(bodyParser.text());
 app.use(express.static(__dirname + '/dist'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.listen(process.env.PORT || 8080);
 
 app.post('/getty_images/search', upload.array(), function (req, res, next) {
